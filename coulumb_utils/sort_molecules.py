ac26bdc91f9e04ea229aac43a3d57d5031cd6fe8
Petko Petkov <petko.petkov987@gmail.com>
2024-12-05 10:56:49 +0100

    sort molecules

---
+++ coulumb_utils/sort_molecules.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sort_by_row_norm(matrix):
+    row_norms = np.linalg.norm(matrix, axis=1)
+    sorted_indices = np.argsort(-row_norms)
+    sorted_matrix = matrix[sorted_indices][:, sorted_indices]
+    return sorted_matrix
+
+def sort_by_atomic_number(atomic_numbers, matrix):
+    sorted_indices = np.argsort(-atomic_numbers)
+    sorted_matrix = matrix[sorted_indices][:, sorted_indices]
+    return sorted_matrix
